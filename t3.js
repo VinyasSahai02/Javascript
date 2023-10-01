02feaa2ca3797e4c79e22a9378d7f1c6d2fd0251
VinyasSahai02 <142114422+VinyasSahai02@users.noreply.github.com>
2023-10-01 18:12:57 +0530

    Add files via upload

---
+++ t3.js
@@ -0,0 +1,31 @@
+//FUNCTIONS
+function cookMaggi(maggi,pani,tapeli){
+    console.log('maggi will be ready in'+maggi*2+'minutes')
+    console.log('ingredients are:'
+    +maggi+'maggi'
+    +pani+'cups of water'
+    +'using'+tapeli+'pan')
+}
+cookMaggi(4,8,1);
+
+//let bread1=prompt('which type of bread would you like');
+//let veggies1=prompt('which type of veggies would you like');
+//let sauce1=prompt('which type of sauces would you like');
+function makeSandwitch(bread,veggies,sauce){
+    let sandwitch= bread+"bread" +veggies+"veggies" +sauce+"sauce";
+    return sandwitch
+}
+let vegSandwitch=makeSandwithc(bread1,veggies1,sauce1);
+console.log(vegSandwitch);
+
+
+//STRINGS
+let str='You\'ve got a string';
+str.length();
+str[0];
+str.indexOf("got")  //8
+str.slice(8,11)  //got
+str.toUpperCase()
+
+
+//CORESION AND CONVERSION
